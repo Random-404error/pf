fd78aa6ff77185b25ba34f409b86a280c5b309bb
Random <rc786986@gmail.com>
2024-04-29 01:10:13 +0530

    some add

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://github.com/RandomFirstOrg/Photos/blob/main/Screenshot%202024-01-24%20204844.png?raw=true",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://github.com/RandomFirstOrg/Photos/blob/main/Screenshot%202024-01-24%20204922.png?raw=true",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://github.com/RandomFirstOrg/Photos/blob/main/Screenshot%202024-01-24%20204922.png?raw=true",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://github.com/RandomFirstOrg/Photos/blob/main/logo_simple.png?raw=true",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  
